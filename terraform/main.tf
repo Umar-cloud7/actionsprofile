b882f5496fe0f9f07798544a81e5a66c8baebd31
Umar-cloud7 <umarsk0209@gmail.com>
2026-07-14 14:10:29 +0530

    configured security group to allow access from ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,17 +23,51 @@ resource "aws_security_group" "all_in_one_sg" {
   vpc_id      = aws_vpc.vprofile_vpc.id
 
   # HTTP
-  ingress { from_port = 80; to_port = 80; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
+  ingress { 
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  
   # Jenkins
-  ingress { from_port = 8080; to_port = 8080; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
+  ingress { 
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  
   # Nexus
-  ingress { from_port = 8081; to_port = 8081; protocol = "tcp"; cidr_blocks = [var.your_ip] }
+  ingress { 
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = [var.your_ip] 
+  }
+  
   # SonarQube
-  ingress { from_port = 9000; to_port = 9000; protocol = "tcp"; cidr_blocks = [var.your_ip] }
+  ingress { 
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = [var.your_ip] 
+  }
+  
   # SSH
-  ingress { from_port = 22; to_port = 22; protocol = "tcp"; cidr_blocks = [var.your_ip] }
+  ingress { 
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.your_ip] 
+  }
 
-  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
+  egress { 
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
 }
 
 # --- Key Pair ---
