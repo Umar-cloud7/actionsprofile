8d620117afdb11f97d38ded5340c8323b299fdc1
Umar-cloud7 <umarsk0209@gmail.com>
2026-07-14 14:17:58 +0530

    added VPC

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,16 @@ terraform {
 }
 
 provider "aws" { region = var.aws_region }
+# --- VPC Definition ---
+resource "aws_vpc" "vprofile_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
 # --- Security Group ---
 resource "aws_security_group" "all_in_one_sg" {
   name        = "${var.project_name}-all-in-one-sg"
